bca4bd227189ec64ce9bf50d06f1408fabb63a72
AlexanderAllen <14018885+AlexanderAllen@users.noreply.github.com>
2020-05-14 10:19:22 -0400

    Disable widevine flags on launch script.

--- run.bat
+++ run.bat
@@ -4,17 +4,18 @@
 
 :: Documentation https://www.electronjs.org/docs/tutorial/testing-widevine-cdm
 
-@echo off
+:: @echo off
 
 
-set widevine-cdm-path=C:\Program Files (x86)\Google\Chrome\Application\81.0.4044.138\WidevineCdm\_platform_specific\win_x64\widevinecdm.dll
-set widevine-cdm-version=4.10.1610.0
+:: set widevine-cdm-path=C:\Program Files (x86)\Google\Chrome\Application\81.0.4044.138\WidevineCdm\_platform_specific\win_x64\widevinecdm.dll
+:: set widevine-cdm-version=4.10.1610.0
 
-echo "Using CDM path %widevine-cdm-path%"
+:: echo "Using CDM path %widevine-cdm-path%"
 
-@echo on
+:: @echo on
 
 npx electron --enable-logging --v=0 ^
---widevine-cdm-path="%widevine-cdm-path%" ^
---widevine-cdm-version=%widevine-cdm-version% ^
 .
+
+::--widevine-cdm-path="%widevine-cdm-path%" ^
+::--widevine-cdm-version=%widevine-cdm-version% ^
